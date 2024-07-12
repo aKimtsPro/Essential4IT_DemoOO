26825f2ff5c5307720cf473f78570c83d5b304ff
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-07-12 14:19:09 +0200

    post démo

--- DemoOO/Models/Auteur.cs
+++ DemoOO/Models/Auteur.cs
@@ -3,6 +3,38 @@ namespace DemoOO.Models
 {
     internal class Auteur
     {
-        public string Nom;
+        public string? Nom { get; set; }
+
+        public Auteur(string nom)
+        {
+            Nom = nom;
+        }
+
+        public static Auteur operator + (Auteur a, Auteur b)
+        {
+            // static => this n'est pas utilisable
+            return new Auteur(a.Nom + ' ' + b.Nom);
+        }
+
+        public static Auteur operator + (Auteur a, string b)
+        {
+            // static => this n'est pas utilisable
+            return new Auteur(a.Nom + ' ' + b);
+        }
+
+        // Cas particulier:
+        // Si je défini l'opération true, je dois aussi défini l'opération false
+        public static bool operator true (Auteur auteur)
+        {
+            // static => this n'est pas utilisable
+            return auteur.Nom != null;
+        }
+
+        public static bool operator false(Auteur auteur)
+        {
+            // static => this n'est pas utilisable
+            return auteur.Nom == null;
+        }
+        
     }
 }
--- DemoOO/Program.cs
+++ DemoOO/Program.cs
@@ -10,34 +10,54 @@ public class Program
     private static void Main(string[] args)
     {
 
-        Vehicule v = new Vehicule("mark", "model");
+        // Vehicule v = new Vehicule("mark", "model");
+        //
+        // v.SeDeplacer(15);
+        //
+        // //v.SetKmParcouru(-15); // private
+        // //v.KmParcouru = 50; // set en private
+        //
+        //
+        // v.SeDeplacer(25);
+        //
+        // Console.WriteLine( v.KmParcouru ); // 40
+        //
+        // // v.Marque = ":)";
+        // Console.WriteLine( v.Marque );
+        //
+        // Parking parking = new Parking("adresse", "leParking", 500);
+        //
+        // parking.SeGarer(v, 1);
+        // parking[1] = v;
+        //
+        // Vehicule vGare = parking.RecupVehicule(1);
+        // Vehicule vG = parking[1];
+        // // Vehicule vGare2 = parking[1];
+        //
+        // // parking.VehiculesGarees = new Dictionary<int, Vehicule>(); // set en private
+        //
+        // int a = 5, b = 7;
+        // int c = a + b;
+        //
+        // string sA = "salut ", sB = "ca va?";
+        // string sC = sA + sB;
 
-        v.SeDeplacer(15);
+        Auteur auteurA = new Auteur("Luc");
+        Auteur auteurB = new Auteur("Marie");
 
-        //v.SetKmParcouru(-15); // private
-        //v.KmParcouru = 50; // set en private
+        // Possible uniquement grâce à la redéfinition de '+' dans Auteur
+        Auteur auteurC = auteurA + auteurB;
 
+        Console.WriteLine(auteurA.Nom);
+        Console.WriteLine(auteurB.Nom);
+        Console.WriteLine(auteurC.Nom);
 
-        v.SeDeplacer(25);
+        // Possible uniquement grâce à la redéfinition de 'true' et 'false' dans Auteur
+        if (auteurC)
+        {
+            Console.WriteLine("L'auteur a bien un nom");
+        }
 
-        Console.WriteLine( v.KmParcouru ); // 40
-
-        // v.Marque = ":)";
-        Console.WriteLine( v.Marque );
-
-        Parking parking = new Parking("adresse", "leParking", 500);
-
-        parking.SeGarer(v, 1);
-        parking[1] = v;
-
-        Vehicule vGare = parking.RecupVehicule(1);
-        Vehicule vG = parking[1];
-        // Vehicule vGare2 = parking[1];
-
-        // parking.VehiculesGarees = new Dictionary<int, Vehicule>(); // set en private
-        
-        
-        
 
     }
 }
