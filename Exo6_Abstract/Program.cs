8ab0c28e3d700ce5fc6b20e238e39f774d03d47b
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2024-07-15 16:53:49 +0200

    Correction exo 6

---
+++ Exo6_Abstract/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Exo6_Abstract;
+
+Personne personne = new Personne(); ;
+
+Courant courant = new Courant("BE01", personne);
+Epargne epargne = new Epargne("BE02", personne);
+
+courant.LigneDeCredit = 100;
+courant.Retrait(10); // Solde: -10
+
+courant.AppliquerInteret(); // -10.975
+
+Console.WriteLine(courant.Solde);
+
+courant.Depot(110.975);
+courant.AppliquerInteret(); // 103
+
+Console.WriteLine(courant.Solde);
+
+epargne.Depot(100);
+epargne.AppliquerInteret(); // 104.5
+
+Console.WriteLine(epargne.Solde);
